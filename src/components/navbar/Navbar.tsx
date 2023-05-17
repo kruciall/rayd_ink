b428e962fd7c1b801bb650f9897b64c02e09f39f
Vinson Wong <vinson.wong93@gmail.com>
2023-05-16 20:42:14 -0400

    change navbar font color to black

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { SocialIcon } from 'react-social-icons';
 import './navbar.css';
@@ -7,30 +7,30 @@ const Navbar = () => {
     <div className="nav container">
       <div className="artistname">
         <h1>RAYMOND</h1>
-       
+
       </div>
       <div className="nav__item">
         <div className="categories">
-        <li className='tattoos'>
-          <a href="#tattoos">
-            Tattoos
-          </a>
-        </li>
-        <li className='about'>
-          <a href="#about">
-            About
-          </a>
-        </li>
-        <li className='contact'>
-          <a href="#contact">
-            Contact
-          </a>
-        </li>
+          <li className='tattoos'>
+            <a href="#tattoos">
+              Tattoos
+            </a>
+          </li>
+          <li className='about'>
+            <a href="#about">
+              About
+            </a>
+          </li>
+          <li className='contact'>
+            <a href="#contact">
+              Contact
+            </a>
+          </li>
         </div>
       </div>
-      <SocialIcon className="igIcon" url="https://www.instagram.com/rayd_ink/" bgColor="none" fgColor="black"/>
+        <SocialIcon className="igIcon" url="https://www.instagram.com/rayd_ink/" bgColor="none" fgColor="black" />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
