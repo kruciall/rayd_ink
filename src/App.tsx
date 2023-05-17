96b84df9120747b4f9e6f2b758714cbffbdc6a9c
Vinson Wong <vinson.wong93@gmail.com>
2023-05-16 20:59:26 -0400

    create header component, add tattoo image and ighandle

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import tattooImage from '/Users/vinsonwong/projects/raymondtattoo/raymond/src/assets/rayd-IMAGE.png'
+import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header__container">
+      <img src={tattooImage} alt="tattooimage" className='mainImage' />
+      <div className="ighandle">
+        <h1 className="rayd">rayd_ink</h1>
+        <h2 className="rayd">Chronic Ink</h2>
+      </div>
+    </div>
   )
 }
 
